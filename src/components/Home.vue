fa10bd9668290a3c98387b0354c9206a0de276af
DevilMayCry2333 <1712363499@qq.com>
2019-09-02 09:31:45 +0800

    最新

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,8 +5,6 @@
   <el-button type="primary" @click="register">注册</el-button>
   </div>
 <el-carousel indicator-position="outside">
-
-
     <el-carousel-item v-for="item in 4" :key="item">
       <h3>{{ item }}</h3>
     </el-carousel-item>
